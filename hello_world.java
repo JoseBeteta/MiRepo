9e42ee47a12de41b307d6e75a128ab13d06d8d97
jose <1993jbetetag@gmail.com>
2015-10-07 19:03:08 +0200

    hello_world updated

--- hello_world.java
+++ hello_world.java
@@ -2,7 +2,7 @@
 public class Application {
     public static void main(String[] args) {
         
-        System.out.println("Hello World!");
+        System.out.println("Hello World: To forgive all is as inhuman as to forgive none (Aristòtil). Fortunately we have GIT");
     }
 
 }
